2bff5cad7e8905a0543037b5687e8f051aeef84c
BalladofaDeathmonkey <51802222+BalladofaDeathmonkey@users.noreply.github.com>
2025-03-11 15:38:39 -0500

    passed test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
+let myString = ""
+const repeatString = function(string, numOfTimes) {
+    for (let i = 1; i <= numOfTimes; i++) {
+        myString += string;
+    }
+    console.log(myString);
 };
+repeatString("hey", 3);
 
 // Do not edit below this line
 module.exports = repeatString;
