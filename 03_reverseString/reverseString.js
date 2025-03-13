090ea52dfe8ed442daffaf1be4058971b215ca0b
BalladofaDeathmonkey <51802222+BalladofaDeathmonkey@users.noreply.github.com>
2025-03-13 12:29:06 -0500

    Algo passed all 4 'Jest' tests, moving on to assignment 4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let newString = string.split("");
+    const newStringReversed = newString.reverse();
+    return newStringReversed.join("");
 };
-
+console.log(reverseString("Hello World!"));
 // Do not edit below this line
 module.exports = reverseString;
